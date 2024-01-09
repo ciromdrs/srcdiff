dd94ba686e032c6f12fe91af526865b83539db33
Ciro <ciromoraismedeiros@gmail.com>
2024-01-09 10:21:12 -0300

    Extract Diff2's distance matrix computation algorithm to a new method

--- src/diff2.py
+++ src/diff2.py
@@ -26,22 +26,8 @@ class Diff2:
         Returns the edit distance.
         """
         self._initialize()
-        self._compute_base_row_and_column()
-
-        # Main loop
-        # Fill `t` using row 0 and column 0
-        for i in range(1, self.n + 1):
-            for j in range(1, self.m + 1):
-                # If the characters are equal
-                if self._get_row_char_at(i) == self._get_col_char_at(j):
-                    # Copy the distance from the diagonal
-                    self.matrix[i][j] = self.matrix[i-1][j-1]
-                else:
-                    # Characters differ, try to push b first
-                    if self.matrix[i][j-1] <= self.matrix[i-1][j]:
-                        self.matrix[i][j] = self.matrix[i][j-1] + 1
-                    else:
-                        self.matrix[i][j] = self.matrix[i-1][j] + 1
+        
+        self._compute_distance_matrix()
 
         return self.matrix[self.n][self.m]
 
@@ -100,6 +86,25 @@ class Diff2:
         for k in range(self.m + 1):
             self.matrix[0][k] = k
 
+    def _compute_distance_matrix(self):
+        """
+        Compute the distance matrix.
+        """
+        # First, compute the base distances
+        self._compute_base_row_and_column()
+        # Then, compute the remainder using them
+        for i in range(1, self.n + 1):
+            for j in range(1, self.m + 1):
+                # If the characters are equal
+                if self._get_row_char_at(i) == self._get_col_char_at(j):
+                    # Copy the distance from the diagonal
+                    self.matrix[i][j] = self.matrix[i-1][j-1]
+                else:
+                    # Characters differ, try to push b first
+                    if self.matrix[i][j-1] <= self.matrix[i-1][j]:
+                        self.matrix[i][j] = self.matrix[i][j-1] + 1
+                    else:
+                        self.matrix[i][j] = self.matrix[i-1][j] + 1
 
 # FUNCTIONS
 
