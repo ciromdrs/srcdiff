678cc94525d06123c1e28df61a63d6487c1e9b25
Ciro <ciromoraismedeiros@gmail.com>
2024-01-13 15:21:35 -0300

    Set EMPTY constant to None
    Using special characters to represent the empty space is not a good option because they are not printable or already have a well-defined behavior in the print function.
    Using non-printable characters is neither a good idea because we cannot see them, so debugging gets harder.
    The None value seems the best choice.

--- src/srcdiff/__init__.py
+++ src/srcdiff/__init__.py
@@ -1,8 +1,11 @@
 # CONSTANTS
 
 # Empty space to fill a diff
-# I believe this is the best character to represent the empty string.
-EMPTY = f'\0'
+# Using special characters to represent the empty space is not a good option because they are not printable or already have a well-defined behavior in the print function.
+# Using non-printable characters is neither a good idea because we cannot see them, so debugging gets harder.
+# The None value seems the best choice.
+EMPTY = None
+
 
 def main():
     raise Exception('Not implemented yet')
