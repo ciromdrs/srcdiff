61217b4ca9e5e8637569201bc9148bbe45c3094c
Ciro <ciromoraismedeiros@gmail.com>
2024-02-08 16:43:56 -0300

    Start implementation of TreeDiff2

---
+++ src/srcdiff/treediff2.py
@@ -0,0 +1,49 @@
+'''Perform diffs on Trees.'''
+
+
+from src.srcdiff import EMPTY
+from src.srcdiff.tree import Tree
+
+
+# CLASSES
+
+class TreeDiff2:
+    def __init__(self, a: Tree, b: Tree):
+        self.a = a
+        self.b = b
+
+    def run(self) -> int: # TODO: return the Tree diffs
+        self._initialize_treedist()
+        # Compute keyroots
+        keyrootsa = self.a.keyroots()
+        keyrootsb = self.b.keyroots()
+        # Compute tree distance between each pair of keyroots
+        for kra in keyrootsa:
+            for krb in keyrootsb:
+                self._treedist(self.a.index_of[kra], self.b.index_of[krb])
+        return self.table[len(self.a)][len(self.b)]
+
+    def _treedist(self, ikra: int, ikrb: int):
+        """Computes the tree edit distance between the subtrees rooted at `ikra` and `ikrb`.
+        `ikra` and `ikrb` are the indices of the keyroots `a` and `b`, respectively."""
+        assert self.table[ikra][ikrb] < 0, 'Should be unitialized.'
+        temp = self._create_temp_edit_table(ikra, ikrb)
+        self.table[ikra][ikrb] = ...
+        return self.table[ikra][ikrb]
+    
+    def _create_temp_edit_table(self, ikra: int, ikrb: int) -> list[list[int]]:
+        """Creates a temporary table to compute the edit distance between the subtrees rooted at `ikra` and `ikrb`."""
+        # Create a temporary (n+1) x (m+1) tree edit distance table, where n and m are the number elements in the subtree rooted at keyroot a and keyroot b, respectively.
+        n = len(self.a[ikra]) + 1
+        m = len(self.b[ikrb]) + 1
+        temp = [[i for i in range(m)]]
+        for i in range(1, n):
+            temp += [[i] + [-1] * (m-1)]
+        return temp
+
+# FUNCTIONS
+
+def tree_diff2(a: Tree, b: Tree) -> int:
+    '''Performs a diff between Trees `a`and `b`.'''
+    result = TreeDiff2(a, b).run()
+    return result
---
+++ tests/test_treediff2.py
@@ -0,0 +1,81 @@
+"""Tests for the treediff2 script."""
+
+import unittest
+from src.srcdiff import EMPTY
+from src.srcdiff.tree import Tree
+from src.srcdiff.treediff2 import TreeDiff2
+
+
+class TestTreeDiff2(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.example_tree_a = Tree('f', children=[
+            Tree('d', children=[
+                Tree('a'),
+                Tree('c', children=[
+                    Tree('b'),
+                ]),
+            ]),
+            Tree('e'),
+        ])
+
+        self.example_tree_b = Tree('f', children=[
+            Tree('c', children=[
+                Tree('d', children=[
+                    Tree('a'),
+                    Tree('b'),
+                ]),
+            ]),
+            Tree('e'),
+        ])
+
+    def test_init(self):
+        """Test if a TreeDiff2 object is instantiated correctly using the constructor."""
+        a = Tree('a')
+        b = Tree('b')
+        td = TreeDiff2(a, b)
+
+        self.assertEqual(td.a, a)
+        self.assertEqual(td.b, b)
+
+    def test_create_temp_edit_table(self):
+        """Tests if the temporary table to compute the edit distance between the subtrees rooted at `ikra` and `ikrb` is created correctly."""
+        td = TreeDiff2(self.example_tree_a, self.example_tree_b)
+        # The tables below are expected temporary tables
+        t2x2 = [[0, 1], [1, -1]]
+        t3x2 = [[0, 1], [1, -1], [2, -1]]
+        t3x7 = [
+            [0,  1,  2,  3,  4,  5,  6],
+            [1, -1, -1, -1, -1, -1, -1],
+            [2, -1, -1, -1, -1, -1, -1]
+        ]
+        t2x7 = [[0, 1, 2, 3, 4, 5, 6], [1, -1, -1, -1, -1, -1, -1]]
+        t7x2 = [[0, 1], [1, -1], [2, -1], [3, -1], [4, -1], [5, -1], [6, -1]]
+        t7x7 = [
+            [0,  1,  2,  3,  4,  5,  6],
+            [1, -1, -1, -1, -1, -1, -1],
+            [2, -1, -1, -1, -1, -1, -1],
+            [3, -1, -1, -1, -1, -1, -1],
+            [4, -1, -1, -1, -1, -1, -1],
+            [5, -1, -1, -1, -1, -1, -1],
+            [6, -1, -1, -1, -1, -1, -1]
+        ]
+        # Keyroots of A are 3, 5, 6
+        # Keyroots of B are 2, 5, 6
+        # The tests explore all the possibilities (cartesian product)
+        test_data = [
+            [3, 2, t3x2],
+            [3, 5, t3x2],
+            [3, 6, t3x7],
+            [5, 2, t2x2],
+            [5, 5, t2x2],
+            [5, 6, t2x7],
+            [6, 2, t7x2],
+            [6, 5, t7x2],
+            [6, 6, t7x7],
+        ]
+
+        for ikra, ikrb, expected in test_data:
+            with self.subTest(f'Keyroots: A={ikra}, B={ikrb}'):
+                temp = td._create_temp_edit_table(ikra, ikrb)
+                self.assertEqual(temp, expected)
