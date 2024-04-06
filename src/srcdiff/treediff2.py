dbd871ee489d30d8faac1985c1826892dc81f9be
Ciro <ciromoraismedeiros@gmail.com>
2024-04-06 19:17:56 -0300

    Implement TreeDiff2._treedist method (possible bug)
    The resulting 6x6 forest distance table is different from the paper* where I got this algorithm from.
    In the paper, the 6x6 table is
    t6_6 = [[0, 1, 2, 3, 4, 5, 6],
            [1, 0, 1, 2, 3, 4, 5],
            [2, 1, 0, 1, 2, 3, 4],
            [3, 2, 1, 2, 3, 4, 5],
            [4, 3, 2, 1, 2, 3, 4],
            [5, 4, 3, 2, 3, 2, 3],
            [6, 5, 4, 3, 3, 3, 2]]
    (lines 3, 4, and 5 changed, starting at position [3,3]).
    Apparently that is because it is not allowed to replace node c by node d.
    Gotta find out why.
    
    * ZHANG, K. and SHASHA, D. Simple Fast Algorithms For the Editing Distance Between Trees and Related Problems. 1989. Available at: https://grantjenks.com/wiki/_media/ideas/simple_fast_algorithms_for_the_editing_distance_between_tree_and_related_problems.pdf

--- src/srcdiff/treediff2.py
+++ src/srcdiff/treediff2.py
@@ -11,40 +11,88 @@ class TreeDiff2:
     def __init__(self, a: Tree, b: Tree):
         self.a = a
         self.b = b
+        self.table: list[list[int]] = self._create_edit_distance_table(
+            len(self.a), len(self.b))
 
-    def run(self) -> int: # TODO: return the Tree diffs
-        self._initialize_treedist()
+    def run(self) -> int:  # TODO: return the Tree diffs
+        """Runs the tree diff algorithm."""
+        self.table = self._create_edit_distance_table(
+            len(self.a), len(self.b))
         # Compute keyroots
         keyrootsa = self.a.keyroots()
         keyrootsb = self.b.keyroots()
         # Compute tree distance between each pair of keyroots
         for kra in keyrootsa:
             for krb in keyrootsb:
-                self._treedist(self.a.index_of[kra], self.b.index_of[krb])
+                self._treedist(kra, krb)
         return self.table[len(self.a)][len(self.b)]
 
-    def _treedist(self, ikra: int, ikrb: int):
-        """Computes the tree edit distance between the subtrees rooted at `ikra` and `ikrb`.
-        `ikra` and `ikrb` are the indices of the keyroots `a` and `b`, respectively."""
-        assert self.table[ikra][ikrb] < 0, 'Should be unitialized.'
-        temp = self._create_temp_edit_table(ikra, ikrb)
-        self.table[ikra][ikrb] = ...
-        return self.table[ikra][ikrb]
-    
-    def _create_temp_edit_table(self, ikra: int, ikrb: int) -> list[list[int]]:
-        """Creates a temporary (n+1) x (m+1) tree edit distance table to compute the edit distance between the subtrees rooted at `ikra` and `ikrb`, where n and m are the number elements in the subtree rooted at keyroot a and keyroot b, respectively, and the +1 is for representing the empty trees.
+    def _treedist(self, kra: Tree, krb: Tree):
+        """Computes the tree edit distance between the subtrees rooted at `kra` and `krb`.
+        `kra` and `krb` are the keyroots `a` and `b`, respectively."""
+        # Get the size of the subtrees
+        n = len(kra)
+        m = len(krb)
+        # Indices of keyroots `a` and `b`, respectively
+        ikra = self.a.index_of[kra]
+        ikrb = self.b.index_of[krb]
+        # Indices of leftmost leaves of keyroots `a` and `b`, respectively
+        ilkra = self.a.index_of[kra.leftmost_leaf()]
+        ilkrb = self.b.index_of[krb.leftmost_leaf()]
+        # Create the edit distance table, the +1's are for representing the empty subtree
+        temp = self._create_edit_distance_table(n+1, m+1)
+        # Initialize the edit distance table
+        for i in range(n+1):
+            temp[i][0] = i
+            for j in range(1, m+1):
+                temp[i][j] = j
+        # Compute the distance between the two subtrees at `kra` and `krb` locally
+        for local_i in range(1, n+1):
+            for local_j in range(1, m+1):
+                # Convert "local" indices from nodes in subtree `kra` to "global" indices in tree `a` (analogous for `b` and `krb`)
+                global_i = self.a.index_of[kra[local_i]]
+                global_j = self.b.index_of[krb[local_j]]
+                # Check if it is a tree or a forest comparison
+                if self.is_tree_comparison(ilkra, ikra, ilkrb, ikrb):
+                    # Check if the nodes are equal to get the edit cost
+                    equal, _, _ = kra[local_i].equals(
+                        krb[local_j], compare_children=False)
+                    # Replace cost
+                    rc = int(not equal)  # Equal nodes cost 0, different nodes cost 1
+                    # Tree comparison
+                    temp[local_i][local_j] = min(
+                        temp[local_i-1][local_j-1] + rc,  # Replace
+                        temp[local_i][local_j-1] + 1,     # Insert
+                        temp[local_i-1][local_j] + 1,     # Remove
+                    )
+                    # Copy tree distances to permanent table
+                    self.table[global_i-1][global_j-1] = temp[local_i][local_j]
+                else:
+                    # Forest comparison
+                    temp[local_i][local_j] = min(
+                        temp[local_i-1][local_j-1] + \
+                            self.table[global_i-1][global_j-1],  # Replace
+                        temp[local_i][local_j-1] + 1,            # Insert
+                        temp[local_i-1][local_j] + 1,            # Remove
+                    )
+        return temp
 
     def is_tree_comparison(self, ia0: int, ia1: int, ib0: int, ib1: int) -> bool:
         a_is_tree = len(self.a.forest(ia0, ia1)) == 1
         b_is_tree = len(self.b.forest(ib0, ib1)) == 1
         return a_is_tree and b_is_tree
+
+    def _create_edit_distance_table(self, n: int, m: int) -> list[list[int]]:
+        """Creates an `n*m` tree edit distance table, where `n` and `m` are the number elements in tree `a` and `b`, respectively.
         """
-        n = len(self.a[ikra]) + 1
-        m = len(self.b[ikrb]) + 1
-        temp = [[i for i in range(m)]]
-        for i in range(1, n):
-            temp += [[i] + [-1] * (m-1)]
-        return temp
+        table = []  # TODO: Create a dict instead
+        for i in range(n):
+            aux = []
+            for _ in range(m):
+                aux += [-1]
+            table += [aux]
+        return table
+
 
 # FUNCTIONS
 
--- tests/test_treediff2.py
+++ tests/test_treediff2.py
@@ -38,62 +38,123 @@ class TestTreeDiff2(unittest.TestCase):
         self.assertEqual(td.a, a)
         self.assertEqual(td.b, b)
 
-    def test_create_temp_edit_table(self):
-        """Tests if the temporary table to compute the edit distance between the subtrees rooted at `ikra` and `ikrb` is created correctly."""
+    def test_create_edit_distance_table(self):
+        """Tests if the edit distance table is created correctly."""
         td = TreeDiff2(self.example_tree_a, self.example_tree_b)
-        # The tables below are expected temporary tables
-        t2x2 = [[0, 1], [1, -1]]
-        t3x2 = [[0, 1], [1, -1], [2, -1]]
-        t3x7 = [
-            [0,  1,  2,  3,  4,  5,  6],
-            [1, -1, -1, -1, -1, -1, -1],
-            [2, -1, -1, -1, -1, -1, -1]
+        # The tables below are expected edit distance tables
+        t1x1 = [[-1]]
+        t1x2 = [[-1, -1]]
+        t2x1 = [[-1],
+                [-1]]
+        t2x2 = [[-1, -1],
+                [-1, -1]]
+        # Data used in the subtests
+        test_data = [
+            [1, 1, t1x1],
+            [1, 2, t1x2],
+            [2, 1, t2x1],
+            [2, 2, t2x2],
         ]
-        t2x7 = [[0, 1, 2, 3, 4, 5, 6], [1, -1, -1, -1, -1, -1, -1]]
-        t7x2 = [[0, 1], [1, -1], [2, -1], [3, -1], [4, -1], [5, -1], [6, -1]]
-        t7x7 = [
-            [0,  1,  2,  3,  4,  5,  6],
-            [1, -1, -1, -1, -1, -1, -1],
-            [2, -1, -1, -1, -1, -1, -1],
-            [3, -1, -1, -1, -1, -1, -1],
-            [4, -1, -1, -1, -1, -1, -1],
-            [5, -1, -1, -1, -1, -1, -1],
-            [6, -1, -1, -1, -1, -1, -1]
-        ]
+        for n, m, expected in test_data:
+            with self.subTest(f'n={n}, m={m}'):
+                created = td._create_edit_distance_table(n, m)
+                self.assertEqual(created, expected)
+
+    def test_treedist(self):
+        """Tests if the tree edit distance is computed correctly."""
+        td = TreeDiff2(self.example_tree_a, self.example_tree_b)
+        # The tables below are expected edit distance tables
+        t3_2 = [[0, 1],
+                [1, 0],
+                [2, 1]]
+        t3_5 = [[0, 1],
+                [1, 1],
+                [2, 2]]
+        t3_6 = [[0, 1, 2, 3, 4, 5, 6],
+                [1, 1, 1, 2, 3, 4, 5],
+                [2, 2, 2, 2, 2, 3, 4]]
+        t5_2 = [[0, 1],
+                [1, 1]]
+        t5_5 = [[0, 1],
+                [1, 0]]
+        t5_6 = [[0, 1, 2, 3, 4, 5, 6],
+                [1, 1, 2, 3, 4, 4, 5]]
+        t6_2 = [[0, 1],
+                [1, 1],
+                [2, 1],
+                [3, 2],
+                [4, 3],
+                [5, 4],
+                [6, 5]]
+        t6_5 = [[0, 1],
+                [1, 1],
+                [2, 2],
+                [3, 3],
+                [4, 4],
+                [5, 4],
+                [6, 5]]
+        ''' BUG: The resulting 6x6 forest distance table is different from the paper* where I got this algorithm from.
+        In the paper, the 6x6 table is
+        t6_6 = [[0, 1, 2, 3, 4, 5, 6],
+                [1, 0, 1, 2, 3, 4, 5],
+                [2, 1, 0, 1, 2, 3, 4],
+                [3, 2, 1, 2, 3, 4, 5],
+                [4, 3, 2, 1, 2, 3, 4],
+                [5, 4, 3, 2, 3, 2, 3],
+                [6, 5, 4, 3, 3, 3, 2]]
+        (lines 3, 4, and 5 changed, starting at position [3,3]).
+        Apparently that is because it is not allowed to replace node c by node d.
+        Gotta find out why.
+
+        * ZHANG, K. and SHASHA, D. Simple Fast Algorithms For the Editing Distance Between Trees and Related Problems. 1989. Available at: https://grantjenks.com/wiki/_media/ideas/simple_fast_algorithms_for_the_editing_distance_between_tree_and_related_problems.pdf
+        '''
+
+        t6_6 = [[0, 1, 2, 3, 4, 5, 6],
+                [1, 0, 1, 2, 3, 4, 5],
+                [2, 1, 0, 1, 2, 3, 4],
+                [3, 2, 1, 1, 1, 2, 3],
+                [4, 3, 2, 1, 2, 2, 3],
+                [5, 4, 3, 2, 2, 2, 3],
+                [6, 5, 4, 3, 3, 3, 2]]
         # Keyroots of A are 3, 5, 6
         # Keyroots of B are 2, 5, 6
         # The tests explore all the possibilities (cartesian product)
         test_data = [
-            [3, 2, t3x2],
-            [3, 5, t3x2],
-            [3, 6, t3x7],
-            [5, 2, t2x2],
-            [5, 5, t2x2],
-            [5, 6, t2x7],
-            [6, 2, t7x2],
-            [6, 5, t7x2],
-            [6, 6, t7x7],
+            [3, 2, t3_2],
+            [3, 5, t3_5],
+            [3, 6, t3_6],
+            [5, 2, t5_2],
+            [5, 5, t5_5],
+            [5, 6, t5_6],
+            [6, 2, t6_2],
+            [6, 5, t6_5],
+            [6, 6, t6_6],
         ]
-
         for ikra, ikrb, expected in test_data:
-            with self.subTest(f'Keyroots: A={ikra}, B={ikrb}'):
-                temp = td._create_temp_edit_table(ikra, ikrb)
-                self.assertEqual(temp, expected)
+            with self.subTest(f'Keyroots A={ikra}, B={ikrb}'):
+                kra = td.a[ikra]
+                krb = td.b[ikrb]
+                computed = td._treedist(kra, krb)
+                self.assertEqual(computed, expected)
 
     def test_is_tree_comparison(self):
-        """Tests the is_tree_comparison  method."""
+        """Tests the is_tree_comparison method."""
         td = TreeDiff2(self.example_tree_a, self.example_tree_b)
         test_data = [
             ['Nodes', 1, 1, 1, 1, True],
             ['Node and subtree', 2, 2, 1, 3, True],
+            ['Subtree and node', 1, 4, 2, 2, True],
             ['Subtrees', 2, 3, 1, 4, True],
             ['Subtree and tree', 2, 3, 1, 6, True],
+            ['Tree and subtree', 1, 6, 1, 3, True],
             ['Trees', 1, 6, 1, 6, True],
             ['Node and subtrees', 1, 1, 1, 5, False],
+            ['Subtrees and node', 1, 5, 1, 1, False],
             ['Subtrees', 1, 5, 1, 5, False],
             ['Subtrees and tree', 1, 5, 1, 6, False],
+            ['Tree and subtrees', 1, 6, 1, 5, False],
         ]
         for desc, ia0, ia1, ib0, ib1, expected in test_data:
-            with self.subTest(f'{desc} ({expected})'):
+            with self.subTest(f'{desc} (a[{ia0}..{ia1}], b[{ib0}..{ib1}]. {expected})'):
                 res = td.is_tree_comparison(ia0, ia1, ib0, ib1)
                 self.assertEqual(res, expected)
